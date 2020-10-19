48c337c09b19fba2d4dba80cdcbdb9e451b7493c
James Kobayashi Junior <james.m.kobayashi@gmail.com>
2020-10-18 23:05:16 -0300

    adicionando a Api de cálculo de custo

--- desafioProximaPorta/app.py
+++ desafioProximaPorta/app.py
@@ -3,6 +3,10 @@ from chalice import Chalice
 from chalice.app import BadRequestError, Response
 import boto3
 import json
+import re
+import dijkstra
+from dijkstra.dijkstra import DijkstraSPF
+from dijkstra.graph import Graph
 
 app = Chalice(app_name='desafioProximaPorta')
 
@@ -38,3 +42,40 @@ def retrievePaths(mapName):
     paths = json.loads(map['Item']['paths']['S'])
     return {'Paths':paths}
     
+@app.route('/costCalculation', methods=['POST'])
+def costCalculation():
+    data = app.current_request.json_body
+    if (not data['mapName'] or not data['startPoint'] or 
+        not data['startPoint'] or not data['autonomy'] or 
+        not data['endPoint'] or not data['valuePerLiter']):
+        BadRequestError('One or more parameters are missing!')
+
+
+    mapName = data['mapName']
+    startPoint = data['startPoint']
+    autonomy = int(data['autonomy'])
+    endPoint = data['endPoint']
+    valuePerLiter = float(data['valuePerLiter'])
+
+
+    
+    paths = retrievePaths(mapName)
+    paths = paths['Paths']
+    regEx = r"\[\w+\,\w+\,\d+\]"
+    paths = list(re.findall(regEx,paths))
+
+    graph = Graph()
+    for i in range(len(paths)):
+        edges = re.findall(r"\w+",paths[i])
+
+        graph.add_edge(edges[0],edges[1],int(edges[2]))
+
+    dijkstra = DijkstraSPF(graph, startPoint)
+    distance = dijkstra.get_distance(endPoint)
+    path = str(" -> ".join(dijkstra.get_path(endPoint)))
+    cost = (distance / autonomy)*valuePerLiter
+    returnText = str("The shortest route from "+ startPoint +" to "+ endPoint
+                    +" is "+ path + " with a total distance of "+ str(distance)
+                    +" and it will cost $"+ str(cost))
+
+    return {'Cost':returnText}
